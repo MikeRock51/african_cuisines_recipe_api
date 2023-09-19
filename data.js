b80e9395d3e461080c33d63b40b76c4028a06f85
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-19 04:07:54 +0100

    Update dataset

--- data.js
+++ data.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
 const data = require("./dataset.json");
 
-data.recipes.forEach((recipe) => {console.log(recipe.recipe_name)});
+// data.recipes.forEach((recipe, index) => {
+    // recipe.recipe_name === 'Kenyan Maharagwe' && console.log(recipe);
+    // fs.appendFileSync('test.json', `${index + 1}. ${recipe.recipe_name}\n`);
+    // console.log(recipe.recipe_name)
+// });
 
 console.log(data.recipes.length);
