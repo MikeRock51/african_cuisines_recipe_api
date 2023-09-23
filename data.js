8fa2224b95e5e31be26ce8c817068babbc1e1265
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-23 09:22:24 +0100

    Check unique data

--- data.js
+++ data.js
@@ -1,10 +1,24 @@
 const fs = require('fs');
 const data = require("./dataset.json");
 
-// data.recipes.forEach((recipe, index) => {
-    // recipe.recipe_name === 'Kenyan Maharagwe' && console.log(recipe);
+const unique = {};
+const filter = []
+
+
+data.recipes.forEach((recipe, index) => {
     // fs.appendFileSync('test.json', `${index + 1}. ${recipe.recipe_name}\n`);
     // console.log(recipe.recipe_name)
-// });
 
-console.log(data.recipes.length);
+    // console.log(recipe.recipe_name)
+    if (unique.hasOwnProperty(recipe.recipe_name)) {
+        unique[recipe.recipe_name] += 1
+    } else {
+        unique[recipe.recipe_name] = 1
+    }
+});
+
+
+
+// console.log(unique);
+// console.log(Object.keys(unique).length);
+// console.log(data.recipes.length);
