24ba2fcfce7ed23371530e343e6fa3b6e5078b24
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-24 04:26:22 +0100

    Remove Duplicate data

--- data.js
+++ data.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const data = require("./dataset.json");
 
 const unique = {};
-const filter = []
+const filtered = []
 
 
-data.recipes.forEach((recipe, index) => {
+data.forEach((recipe, index) => {
     // fs.appendFileSync('test.json', `${index + 1}. ${recipe.recipe_name}\n`);
     // console.log(recipe.recipe_name)
 
@@ -14,11 +14,16 @@ data.recipes.forEach((recipe, index) => {
         unique[recipe.recipe_name] += 1
     } else {
         unique[recipe.recipe_name] = 1
+        filtered.push(recipe)
     }
 });
 
+// fs.writeFileSync('filteredDataset.json', JSON.stringify(filtered));
+// console.log(filtered.length);
 
-
+console.log(Object.keys(unique).length)
 // console.log(unique);
-// console.log(Object.keys(unique).length);
-// console.log(data.recipes.length);
+// Object.keys(unique).forEach((food, index) => {
+//     fs.appendFileSync('foodNames.txt', `${index + 1}. ${food}\n`);
+// });
+console.log(data.length);
