cc5ed2ebf877604d162ddf95cf425fb23b1e14fd
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-26 15:37:51 +0100

    Implement login route

--- api/v1/views/session.py
+++ api/v1/views/session.py
@@ -7,6 +7,11 @@ from api.v1.auth import auth
 from api.v1.utils.utilities import Utilities
 from models import storage
 from models.user import User
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
 
 @app_views.route('/login', methods=['POST'])
 def userLogin():
@@ -17,10 +22,15 @@ def userLogin():
         user = auth.getUser(credential.get('email'))
         if not user.validatePassword(credential.get('password')):
             raise ValueError('Incorrect Password!')
+        response = {
+                "status": "success",
+                "message": "Log in successful",
+                getenv("AUTH_HEADER"): auth.createSession(user.id)
+        }
     except ValueError as e:
         return jsonify({
             "status": "error",
             "message": str(e)
         })
     
-    return jsonify(credential)
+    return jsonify(response), 200
