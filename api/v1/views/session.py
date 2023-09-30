6f51a1d953718c151adfd8a25b8694bc79971b65
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-30 15:29:30 +0100

    Add Swagger doc for /login

--- api/v1/views/session.py
+++ api/v1/views/session.py
@@ -8,15 +8,19 @@ from api.v1.utils import Utils
 from models import storage
 from models.user import User
 from dotenv import load_dotenv
-from os import getenv
+from os import getenv, path
+from flasgger.utils import swag_from
 
 load_dotenv()
+DOCS_DIR = path.abspath('api/v1/views/documentations/session')
 
 
 @app_views.route('/login', methods=['POST'])
+@swag_from(f'{DOCS_DIR}/login.yml')
 def userLogin():
     """Creates a user session"""
     requiredFields = ['email', 'password']
+    detailed = request.args.get('detailed', False)
     try:
         credential = Utils.getReqJSON(request, requiredFields)
         user = auth.getUserEmail(credential.get('email'))
@@ -26,13 +30,13 @@ def userLogin():
             "status": "success",
             "message": "Log in successful",
             getenv("AUTH_HEADER"): auth.createSession(user.id),
-            "data": user.toDict()
+            "data": user.toDict(detailed=detailed)
         }
     except ValueError as e:
         return jsonify({
             "status": "error",
             "message": str(e)
-        })
+        }), 400
 
     return jsonify(response), 200
 
