acdc9b4c680fa8bc09361137dde23b7cbf280f33
MikeRock51 <mikerockmusic51@gmail.com>
2023-10-03 16:41:22 +0100

    Implement  graphql get recipe by id

--- schema/__init__.py
+++ schema/__init__.py
@@ -32,14 +32,22 @@ class User(SQLAlchemyObjectType):
 class Query(graphene.ObjectType):
     """Defines the entry point for querying data from the API"""
     node = graphene.relay.Node.Field()
+    
     recipes = SQLAlchemyConnectionField(Recipe.connection)
+    recipe = graphene.Field(Recipe, id=graphene.String())
+
     users = SQLAlchemyConnectionField(User.connection)
     user = graphene.Field(User, id=graphene.String())
     
 
+    def resolve_recipe(self, info, id):
+        """Fetches a recipe based on ID"""    
+        recipe = RecipeModel.query.filter(RecipeModel.id == id).one()
+        return recipe
+
     @login_required()
     def resolve_user(self, info, id):
-        """Fetches a user based on their ID"""    
+        """Fetches a user based on ID"""    
         user = UserModel.query.filter(UserModel.id == id).one()
         del user._password
 
@@ -47,7 +55,6 @@ class Query(graphene.ObjectType):
             return user
         else:
             abort(401)
-        
 
     @login_required([UserRole.admin])
     def resolve_users(self, info, sort=None):
