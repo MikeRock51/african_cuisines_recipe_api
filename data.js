f67e6fe4d3e1a37c65e0063b65b42a04b1a3fc50
MikeRock51 <mikerockmusic51@gmail.com>
2023-10-05 11:09:17 +0100

    Add server config files

--- data.js
+++ data.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const data = require("./dataset.json");
 
 const unique = {};
-const filtered = [];
 
 data.forEach((recipe, index) => {
   if (recipe.total_time_hours) {
@@ -17,23 +16,8 @@ data.forEach((recipe, index) => {
     recipe.cook_time_minutes = recipe.cook_time_hours * 60
     delete recipe.cook_time_hours
   }
-
-  // if (unique.hasOwnProperty(recipe.recipe_name)) {
-  //   unique[recipe.recipe_name] += 1;
-  // } else {
-  //   unique[recipe.recipe_name] = 1;
-  //   filtered.push(recipe);
-  // }
 });
+
 fs.writeFileSync('filteredDataset.json', JSON.stringify(data));
-// console.log(data[0]);
-
-// fs.writeFileSync('filteredDataset.json', JSON.stringify(filtered));
-// console.log(filtered.length);
-
 console.log(Object.keys(unique).length);
-// console.log(unique);
-// Object.keys(unique).forEach((food, index) => {
-//     fs.appendFileSync('foodNames.txt', `${index + 1}. ${food}\n`);
-// });
 console.log(data.length);
---
+++ server_configurations/fabfile.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Deploys """
+# import collections.abc
+# collections.Mapping = collections.abc.Mapping
+
+from fabric.api import *
+from dotenv import load_dotenv
+from os import getenv
+from datetime import datetime
+
+load_dotenv()
+
+SQL_ROOT_PWD = getenv("SQL_ROOT_PWD")
+PSN = getenv("PSN") 
+
+def set_sql_pwd():
+    """Sets the root password of MySQL"""
+    run(f'echo "mysql-server mysql-server/root_password password {SQL_ROOT_PWD}" | debconf-set-selections')
+    run(f'echo "mysql-server mysql-server/root_password_again password {SQL_ROOT_PWD}" | debconf-set-selections')
+
+def do_pack():
+    """Packs the application file in a .tgz archive"""
+    local("mkdir -p versions")
+
+    dateString = datetime.utcnow().strftime("%Y:%m:%d:%H:%M:%S")
+    path = f"versions/{PSN}.tgz"
+
+    status = local(f"tar -cvzf ")
