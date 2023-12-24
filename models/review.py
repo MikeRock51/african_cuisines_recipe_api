718608ef19de161af203443e94331868caa9647e
MikeRock51 <mikerockmusic51@gmail.com>
2023-12-24 17:26:15 +0100

    Add review model

--- models/review.py
+++ models/review.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Review Module"""
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
     """Defines a review object"""
-    pass
+    title = Column(String(60), nullable=True)
+    description = Column(String(1024), nullable=False)
+    recipeID = Column(String(60), ForeignKey('recipes.id'), nullable=False)
